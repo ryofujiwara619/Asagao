064a820a1de27825f8434bbc2a8d23f9beff4c5a
Ryo Fujiwara <frmtricolore@gmail.com>
2017-08-01 21:16:25 +0900

    Create show_action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,6 +3,10 @@ class MembersController < ApplicationController
     @members = Member.order("number")
   end
 
+  def show
+    @member = Member.find(params[:id])
+  end
+
   def search
     # member.rbのsearchアクションを利用
     @members = Member.search(params[:q])
