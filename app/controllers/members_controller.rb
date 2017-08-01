e35649eb93c7b654ac8bad404af147bbcf39ad3f
Ryo Fujiwara <frmtricolore@gmail.com>
2017-08-01 18:29:56 +0900

    Create search action at controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,4 +2,9 @@ class MembersController < ApplicationController
   def index
     @members = Member.order("number")
   end
+
+  def search
+    @members = Member.search(params[:q])
+    render "index"
+  end
 end
