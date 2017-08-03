11d137218901475c2f1955022e221e9136a3ec03
Ryo Fujiwara <frmtricolore@gmail.com>
2017-08-03 14:15:54 +0900

    Create member#create action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,6 +11,14 @@ class MembersController < ApplicationController
     @member = Member.new(birthday: Date.new(1980, 1, 1))
   end
 
+  def create
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to member_path(@member.id), notine: "会員を登録しました"
+    else
+      render "new"
+  end
+
   def edit
     @member = Member.find(params[:id])
   end
